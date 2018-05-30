ef99afc87814500ccaa4e4c839e0f9eba63603fc
Roman Babanov <r.babanov@bi.zone>
2018-05-30 20:21:49 +0300

    Добавил валидацию в форме создания тестов

--- src/modules/testing/index.vue
+++ src/modules/testing/index.vue
@@ -3,6 +3,16 @@
   import CreateTestForm from './src/com/Test/CreateForm.vue';
   import CreateQuestionForm from './src/com/Question/CreateForm.vue';
 
+  const DEFAULT_QUESTION = {
+    text: '',
+    answers: [
+      {
+        text: '',
+        correct: false
+      }
+    ]
+  };
+
   export default {
     components: {
       CreateTestForm,
@@ -24,21 +34,19 @@
         saving: false,
         loading: false,
         tests: [],
-        question: {
-          text: '',
-          answers: [
-            {
-              text: '',
-              correct: false
-            }
-          ]
-        },
-        selectedTest: null
+        question: DEFAULT_QUESTION,
+        selectedTest: null,
+        userAnswers: [
+
+        ]
       }
     },
     computed: {
       hasSelectedTest() {
         return this.selectedTest !== null;
+      },
+      hasCorrectAnswers() {
+        return this.question.answers.some(answer => answer.correct === true);
       }
     },
     methods: {
@@ -70,7 +78,12 @@
         await this.api.post('createdQuestionUsingPOST', { id }, question);
         this.saving = false;
         this.closeCreateQuestionPopup();
-        this.loadTests();
+        this.clearQuestionForm();
+        await this.loadTests();
+      },
+      clearQuestionForm() {
+        this.question = DEFAULT_QUESTION;
+        this.$refs.questionForm.clearForm()
       },
       selectTest(test) {
         this.selectedTest = test;
@@ -105,9 +118,9 @@
             </div>
         </el-dialog>
         <el-dialog :visible.sync="isShowQuestionPopup" title="Новый вопрос" width="500px">
-            <create-question-form v-model="question"/>
+            <create-question-form v-model="question" ref="questionForm"/>
             <div slot="footer">
-                <el-button type="success" :loading="saving" @click="saveQuestion">Создать</el-button>
+                <el-button type="success" :disabled="!hasCorrectAnswers" :loading="saving" @click="saveQuestion">Создать</el-button>
                 <el-button @click="closeCreateQuestionPopup">Отмена</el-button>
             </div>
         </el-dialog>
@@ -138,6 +151,9 @@
                     <div class="questions-container">
                         <div class="question-wrapper" v-for="question in selectedTest.questions" :key="question.id">
                             <question-card :question="question"/>
+                        </div>
+                        <div class="actions-block">
+                            <el-button type="success" icon="el-icon-circle-check-outline">Отправить ответы</el-button>
                         </div>
                     </div>
                 </template>
@@ -183,14 +199,21 @@
     .questions {
         flex: 1;
         display: flex;
+        position: relative;
         flex-direction: column;
         width: 100%;
         background-color: #eee;
     }
 
     .questions-container {
+        padding-bottom: 60px;
         overflow: auto;
     }
+
+    .question-wrapper {
+        margin: 16px;
+    }
+
 
     .panel {
         background-color: #fff;
@@ -199,7 +222,18 @@
         border-bottom: 1px solid #cacaca;
     }
 
-    .question-wrapper {
-        margin: 16px;
+    .actions-block {
+        display: flex;
+        padding: 0 16px;
+        align-items: center;
+        justify-content: flex-end;
+        position: absolute;
+        z-index: 1000;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 60px;
+        background-color: #fff;
+        border-top: 1px solid #cacaca;
     }
 </style>
--- src/modules/testing/src/com/Question/CreateForm.vue
+++ src/modules/testing/src/com/Question/CreateForm.vue
@@ -1,31 +1,46 @@
 <script>
+  const DEFAULT_QUESTION = {
+    text: '',
+    answers: [
+      {
+        text: '',
+        correct: false
+      }
+    ]
+  };
   export default {
     props: {
       value: {
         type: Object,
         default() {
           return {
-            text: '',
-            answers: [
-              {
-                text: '',
-                correct: false
-              }
-            ]
+            question: DEFAULT_QUESTION
           }
         }
       }
     },
     data () {
       return {
-        question: this.value
+        question: this.value,
+        maxAnswersCount: 4,
+        isShowMaxAnswersCountError: false
       };
+    },
+    computed: {
+      hasMaxAnswersCountError() {
+        return this.question.answers.length === this.maxAnswersCount;
+      }
     },
     watch: {
       question: {
         deep: true,
         handler(val) {
-          this.$emit('input', val)
+          this.$emit('input', val);
+        }
+      },
+      hasMaxAnswersCountError(value) {
+        if (value === false) {
+          this.isShowMaxAnswersCountError = value;
         }
       }
     },
@@ -36,7 +51,16 @@
           correct: false
         };
 
+
+        if (this.hasMaxAnswersCountError) {
+          this.isShowMaxAnswersCountError = true;
+          return;
+        }
+
         this.question.answers = [ ...this.question.answers, newAnswer ];
+      },
+      clearForm() {
+        this.question = Object.assign({}, DEFAULT_QUESTION, { answers: [{ text: '', correct: false } ]});
       },
       removeAnswerByIndex(removeIndex) {
         const answers = this.question.answers;
@@ -44,6 +68,7 @@
         if (answers.length <= 1) {
           return;
         }
+
         this.question.answers = answers.filter((answer,idx) => idx !== removeIndex);
       }
     }
@@ -53,18 +78,31 @@
 <template>
     <div class="form">
         <div class="form-item">
-            <div class="title">Вопрос</div>
-            <el-input type="textarea" :rows="3" v-model="question.text"/>
+            <el-input type="textarea" placeholder="Текст вопроса" :rows="3" v-model="question.text"/>
         </div>
         <div class="form-item">
             <div class="title">Варианты ответов</div>
-            <div class="answer" v-for="(answer,idx) in question.answers">
-                <el-input v-model="answer.text"/>
-                <el-checkbox v-model="answer.correct">Правильный</el-checkbox>
-                <i class="el-icon-delete delete-icon" @click="removeAnswerByIndex(idx)"/>
+            <div class="answer" v-for="(answer,idx) in question.answers" :key="answer.id">
+                <el-input v-model="answer.text">
+                    <el-button
+                        :key="idx"
+                        slot="append"
+                        type="danger"
+                        icon="el-icon-delete"
+                        @click="removeAnswerByIndex(idx)"
+                    />
+                </el-input>
+                <el-switch
+                    v-model="answer.correct"
+                    active-color="#13ce66"
+                    inactive-color="#ff4949"
+                    active-text="Правильный"
+                />
+                <!--<i class="el-icon-delete delete-icon"/>-->
             </div>
             <div class="action-button">
-                <el-button type="primary" @click="addAnswer">Добавить вариант</el-button>
+                <el-button type="primary" @click="addAnswer" size="mini">Добавить вариант</el-button>
+                <el-tag v-if="isShowMaxAnswersCountError" size="medium">Максимальное количество ответов {{maxAnswersCount}}</el-tag>
             </div>
         </div>
     </div>
@@ -102,6 +140,14 @@
 
     .action-button {
         padding: 16px 0;
+    }
+
+    .checkbox-title {
+        font-size: 12px;
+    }
+
+    .el-tag {
+        margin-left: 16px;
     }
 
 </style>
