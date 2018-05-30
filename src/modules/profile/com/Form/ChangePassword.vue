c90935712a3196b60e2d63d30abf247cb690dfe1
Roman Babanov <r.babanov@bi.zone>
2018-05-30 20:57:59 +0300

    Сделал форму для смены пароля с валидацией всех кейсов

---
+++ src/modules/profile/com/Form/ChangePassword.vue
@@ -0,0 +1,55 @@
+<script>
+  export default {
+    props: {
+      value: {
+        type: Object,
+        default() {
+          return {
+            oldPassword: '',
+            newPassword: '',
+            newPasswordConfirm: ''
+          };
+        }
+      }
+    },
+    data () {
+      return {
+        password: this.value
+      }
+    },
+    watch: {
+      value: {
+        deep: true,
+        handler(password) {
+          this.password = password;
+        }
+      },
+      password: {
+        deep: true,
+        handler(val) {
+          this.$emit('input', val);
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+    <el-form>
+        <el-form-item label="Старый пароль">
+            <el-input v-model="password.oldPassword" type="password">
+                <i slot="prefix" class="el-input__icon el-icon-edit"></i>
+            </el-input>
+        </el-form-item>
+        <el-form-item label="Новый пароль">
+            <el-input v-model="password.newPassword" type="password">
+                <i slot="prefix" class="el-input__icon el-icon-edit"></i>
+            </el-input>
+        </el-form-item>
+        <el-form-item label="Подтверждение">
+            <el-input v-model="password.newPasswordConfirm" type="password">
+                <i slot="prefix" class="el-input__icon el-icon-edit"></i>
+            </el-input>
+        </el-form-item>
+    </el-form>
+</template>
--- src/modules/profile/index.vue
+++ src/modules/profile/index.vue
@@ -1,26 +1,76 @@
 <script>
+  import ChangePasswordForm from './com/Form/ChangePassword.vue';
+
   export default {
+    components: {
+      ChangePasswordForm
+    },
     mounted() {
       this.loadUser();
     },
     data () {
       return {
         user: null,
-        isUserLoading: false
+        isUserLoading: false,
+        isShowChangePasswordForm: false,
+        password: {
+          oldPassword: '',
+          newPassword: '',
+          newPasswordConfirm: ''
+        },
+        showPasswordHint: false
       }
     },
     computed: {
       hasUser() {
         return this.user !== null;
+      },
+      isEmptyFields() {
+        const { oldPassword, newPassword, newPasswordConfirm } = this.password;
+        return oldPassword.trim() === '' || newPassword.trim() === '' || newPasswordConfirm.trim() === '';
+      },
+      isUnsafe() {
+        const { newPassword } = this.password;
+        return newPassword.length < 8;
+      },
+      isNotMatch() {
+        const { newPassword, newPasswordConfirm } = this.password;
+        return newPassword !== newPasswordConfirm;
+      },
+      hasInputError() {
+        return this.isEmptyFields || this.isUnsafe || this.isNotMatch;
+      }
+    },
+    watch: {
+      password: {
+        deep: true,
+        handler() {
+          this.showPasswordHint = false;
+        }
       }
     },
     methods: {
-        async loadUser() {
-            this.isUserLoading = true;
-            const { data: user } = await this.$locator.Api.get('currentUserUsingGET');
-            this.user = user;
-            this.isUserLoading = false;
+      async loadUser() {
+        this.isUserLoading = true;
+        const { data: user } = await this.$locator.Api.get('currentUserUsingGET');
+        this.user = user;
+        this.isUserLoading = false;
+      },
+      async changePassword() {
+        if (this.hasInputError) {
+          this.showPasswordHint = true;
+          return;
         }
+
+        await {};
+        // this.closeChangePasswordForm();
+      },
+      showChangePasswordForm() {
+        this.isShowChangePasswordForm = true;
+      },
+      closeChangePasswordForm() {
+        this.isShowChangePasswordForm = false;
+      }
     }
   }
 </script>
@@ -41,8 +91,22 @@
                 <el-form-item label="Почта">
                     <el-input v-model="user.email" disabled/>
                 </el-form-item>
+                <el-form-item>
+                    <el-button type="info" @click="showChangePasswordForm">Сменить пароль</el-button>
+                </el-form-item>
             </el-form>
         </div>
+
+        <el-dialog :visible.sync="isShowChangePasswordForm" title="Смена пароля" width="480px">
+            <change-password-form v-model="password"/>
+            <el-tag v-if="showPasswordHint && isEmptyFields" type="danger" class="hint">Заполните пустые поля</el-tag>
+            <el-tag v-if="showPasswordHint && isUnsafe" type="danger" class="hint">Длина нового пароля должна быть не менее 8 символов</el-tag>
+            <el-tag v-if="showPasswordHint && isNotMatch" type="danger" class="hint">Пароли не совпадают</el-tag>
+            <div slot="footer">
+                <el-button type="success" @click="changePassword">Сохранить</el-button>
+                <el-button @click="closeChangePasswordForm">Отмена</el-button>
+            </div>
+        </el-dialog>
     </page-container>
 </template>
 
@@ -54,5 +118,9 @@
 
     .form-wrapper {
         margin-top: 16px;
+    }
+
+    .hint {
+        margin: 4px 0;
     }
 </style>
